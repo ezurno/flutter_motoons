14e1f087c3033bff6023e79a0c3780eaf083e75a
ezurno <ezurno@gmail.com>
2023-03-25 19:08:39 +0900

    SET: widget refactoring
    1. widget 을 찢어서 가독성이 좋아지게 함
    
    2. value 값을 받아오면 해결

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_motoons/models/webtoons_model.dart';
 import 'package:flutter_motoons/services/api_service.dart';
+import 'package:flutter_motoons/widgets/webtoons_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -70,42 +71,10 @@ class HomeScreen extends StatelessWidget {
 
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              // 자식 widget의 영역을 관리해줌 >> 넘치는 화면을 hardEdge로 잘라줌
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    offset: const Offset(10, 10),
-                    blurRadius: 15,
-                  ), // BoxShadow widget은 Box의 그림자를 만들어주는 Widget
-                ],
-              ),
-              child: Image.network(
-                webtoon.thumb,
-                headers: const {
-                  "User-Agent":
-                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-                },
-                // Image.network 는 url 의 이미지를 가져옴
-                // 하지만 브라우저에서 브라우저값이 아니면 차단하기 때문에 User-Agent 값을 넣어주면 해결 됨
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
         );
       },
 
