e30725583ed333a6ba6947bd4d23dc0f91789ebc
ezurno <ezurno@gmail.com>
2023-03-26 20:50:49 +0900

    LEARN: Hero-widget
    1. Hero-widget 은 react-animation 의 layoutId 를 부여해서 anmation을 주는 것과 유사함
    
    2. 지정할 widget을 Hero-widget으로 감싸서 사용하며 tag를 지정해주어야 함
    
    3. 다른 화면의 해당 widget 에도 동일한 tag 값을 주면 화면 전환시 해당 widget이 전환되지 않고 이동하는 애니메이션을 줌

--- lib/widgets/webtoons_widget.dart
+++ lib/widgets/webtoons_widget.dart
@@ -46,28 +46,33 @@ class Webtoon extends StatelessWidget {
 
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            // 자식 widget의 영역을 관리해줌 >> 넘치는 화면을 hardEdge로 잘라줌
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: const Offset(10, 10),
-                  blurRadius: 15,
-                ), // BoxShadow widget은 Box의 그림자를 만들어주는 Widget
-              ],
-            ),
-            child: Image.network(
-              thumb,
-              headers: const {
-                "User-Agent":
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-              },
-              // Image.network 는 url 의 이미지를 가져옴
-              // 하지만 브라우저에서 브라우저값이 아니면 차단하기 때문에 User-Agent 값을 넣어주면 해결 됨
+          Hero(
+            // Hero-widget 은 react-animation 의 layoutid 를 사용하는 것과 유사함
+            tag: id,
+            // tag 는 해당 widget 의 id를 부여하는 값
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              // 자식 widget의 영역을 관리해줌 >> 넘치는 화면을 hardEdge로 잘라줌
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    offset: const Offset(10, 10),
+                    blurRadius: 15,
+                  ), // BoxShadow widget은 Box의 그림자를 만들어주는 Widget
+                ],
+              ),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+                },
+                // Image.network 는 url 의 이미지를 가져옴
+                // 하지만 브라우저에서 브라우저값이 아니면 차단하기 때문에 User-Agent 값을 넣어주면 해결 됨
+              ),
             ),
           ),
           const SizedBox(
