61b569d1c68c2e2e7920099f824c682a45927a71
ezurno <ezurno@gmail.com>
2023-04-23 21:19:03 +0900

    SET: used webtoon-detail data
    1. api 에서 받아온 webtoon 의 id를 넘겨서 받아온 detail data를 사용
    
    2. padding을 주어 thumnail 과 간격이 비슷하게 유지

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -88,6 +88,46 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            // webtoon 과 episodes 를 가져오기 위해 사용
+
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const Text("...");
+              }
+            },
+            // widget 을 return gksms builder.
+          )
         ],
       ),
     );
