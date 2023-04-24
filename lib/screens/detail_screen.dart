767a8ff03c2b87131679e9c51e2e9259e17d83fd
ezurno <ezurno@gmail.com>
2023-04-24 23:33:23 +0900

    SET: SingleChildScrollView()
    1. 화면이 overflow 되어 제대로 나오지 않을 때 SingleChildScrollView() widget으로 감싸줌
    
    2. 제일 큰 widget을 감싸주어야 함

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -51,107 +51,121 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        // 화면이 overflow 될 때 감싸주면 넘치는 화면을 제대로 구사 할 수 있음
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  // 자식 widget의 영역을 관리해줌 >> 넘치는 화면을 hardEdge로 잘라줌
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.5),
-                        offset: const Offset(10, 10),
-                        blurRadius: 15,
-                      ), // BoxShadow widget은 Box의 그림자를 만들어주는 Widget
-                    ],
-                  ),
-                  child: Image.network(
-                    widget.thumb,
-                    headers: const {
-                      "User-Agent":
-                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-                    },
-                    // Image.network 는 url 의 이미지를 가져옴
-                    // 하지만 브라우저에서 브라우저값이 아니면 차단하기 때문에 User-Agent 값을 넣어주면 해결 됨
-                  ),
-                ),
+              const SizedBox(
+                height: 50,
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      // 자식 widget의 영역을 관리해줌 >> 넘치는 화면을 hardEdge로 잘라줌
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            offset: const Offset(10, 10),
+                            blurRadius: 15,
+                          ), // BoxShadow widget은 Box의 그림자를 만들어주는 Widget
+                        ],
+                      ),
+                      child: Image.network(
+                        widget.thumb,
+                        headers: const {
+                          "User-Agent":
+                              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+                        },
+                        // Image.network 는 url 의 이미지를 가져옴
+                        // 하지만 브라우저에서 브라우저값이 아니면 차단하기 때문에 User-Agent 값을 넣어주면 해결 됨
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                // webtoon 과 episodes 를 가져오기 위해 사용
+
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Text("...");
+                  }
+                },
+                // widget 을 return gksms builder.
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              FutureBuilder(
+                future: episodes,
+                // episodes 를 받아오기 위해
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        for (var episode in snapshot.data!.length > 8
+                            ? snapshot.data!.sublist(0, 8)
+                            : snapshot.data!)
+                          Container(
+                            decoration:
+                                BoxDecoration(color: Colors.green.shade300),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child: Row(
+                                children: [
+                                  Text(episode.title),
+                                  const Icon(Icons.chevron_right_rounded)
+                                ],
+                              ),
+                            ),
+                          )
+                      ],
+                    );
+                  }
+
+                  // 만약 리스트의 길이를 모르면 ListView() 로 생성하는 편이 좋음
+
+                  return Container();
+                },
+              )
             ],
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            // webtoon 과 episodes 를 가져오기 위해 사용
-
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snapshot.data!.about,
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              } else {
-                return const Text("...");
-              }
-            },
-            // widget 을 return gksms builder.
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: episodes,
-            // episodes 를 받아오기 위해
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  children: [
-                    for (var episode in snapshot.data!.length > 8
-                        ? snapshot.data!.sublist(0, 8)
-                        : snapshot.data!)
-                      Text(episode.title),
-                  ],
-                );
-              }
-
-              // 만약 리스트의 길이를 모르면 ListView() 로 생성하는 편이 좋음
-
-              return Container();
-            },
-          )
-        ],
+        ),
       ),
     );
   }
