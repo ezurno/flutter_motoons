474e9027227a0e0978d692cb18837663ee45fa4a
ezurno <ezurno@gmail.com>
2023-04-25 20:36:23 +0900

    SET: refactoring & import url launcher
    1. Episode-widget 으로 따로 리팩토링
    
    2. 그 후 Episode widget 내에 url 연결

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_motoons/models/webtoon_episode_model.dart';
 import 'package:flutter_motoons/services/api_service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/webtoon_detail_model.dart';
 
@@ -139,34 +140,7 @@ class _DetailScreenState extends State<DetailScreen> {
                             : snapshot.data!)
                           // 최근 8편의 웹툰만 노출 되게 끔 조건문 작성
 
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.green.shade400,
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 10),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
+                          Episode(episode: episode)
                       ],
                     );
                   }
@@ -178,6 +152,51 @@ class _DetailScreenState extends State<DetailScreen> {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+  });
+
+  final WebtoonEpisodeModel episode;
+
+  onButtonTap() async {
+    final url = Uri.parse("https://www.google.com/");
+    await launchUrl(url);
+    // luanchUrl 은 Future 를 갖는 함수기 때문에 onButtonTap 에 async 를 걸어야 함
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.green.shade400,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              episode.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+            const Icon(
+              Icons.chevron_right_rounded,
+              color: Colors.white,
+            )
+          ],
         ),
       ),
     );
